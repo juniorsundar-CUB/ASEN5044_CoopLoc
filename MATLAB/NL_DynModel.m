76c568e72f80f62b8f2f42bf4de44d5164c3e5e5
Junior Sundar <juniorsundar@gmail.com>
2020-11-21 15:41:26 -0700

    Updated NL Models and Q3

--- MATLAB/NL_DynModel.m
+++ MATLAB/NL_DynModel.m
@@ -1,7 +1,33 @@
-function [outputArg1,outputArg2] = NL_DynModel(inputArg1,inputArg2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function xdot = NL_DynModel(t,x,u,Pnoise)
+%NL_DynModel
+%   input: t - time model; x - state vector; u - control input vector;
+%          Pnoise - process noice vector
+%   output: ___
+%   Function input for ode45 for NL dynamics model
+
+% u = [v_g, phi_g, v_a, w_a]';
+% x = [xi_g eta_g theta_g xi_a eta_a theta_a]';
+% ~w = [w_x,g w_y,g w_w,g w_x,a w_y,a w_w,a]';
+L = 0.5;
+
+xdot = [u(1)*cos(x(3)) + Pnoise(1);
+        u(1)*sin(x(3)) + Pnoise(2);
+        (u(1)/L)*(tan(u(2))) + Pnoise(3);
+        u(3)*cos(x(6)) + Pnoise(4);
+        u(3)*sin(x(6)) + Pnoise(5);
+        u(4) + Pnoise(6)];
+    
+% if x(3)>pi
+%     x(3) = -pi + (x(3)-pi);
+% elseif x(3)<-pi
+%     x(3) = pi - (x(3)+pi) ;
+% end
+% 
+% if x(6)>pi
+%     x(6) = -pi + (x(6)-pi);
+% elseif x(6)<-pi
+%     x(6) = pi - (x(6)+pi) ;
+% end
+
 end
 
