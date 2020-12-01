7257f4665ef2e2e3eed8699275def10d1dab5de4
Junior Sundar <juniorsundar@gmail.com>
2020-11-30 17:57:11 -0700

    Tuned LKF + IMP: REMOVED ANG. WRAP GenerateTruth.m

--- MATLAB/GenerateTruth.m
+++ MATLAB/GenerateTruth.m
@@ -10,8 +10,8 @@ y = zeros(p,steps+1);
 % set initial conditions
 dx = mvnrnd(zeros(1,n),P0);
 x(:,1) = x0 + dx';
-x(3,1) = wrapToPi(x(3,1));
-x(6,1) = wrapToPi(x(6,1));
+% x(3,1) = wrapToPi(x(3,1));
+% x(6,1) = wrapToPi(x(6,1));
 
 for i = 2:steps+1
     
@@ -24,8 +24,8 @@ for i = 2:steps+1
     [~,next_x] = ode45(@NL_DynModel, [0 Dt], x(:,i-1)', opt, u', wk);
     
     % wrap angle to [-pi pi]
-    next_x(3) = wrapToPi(next_x(3));
-    next_x(6) = wrapToPi(next_x(6));
+%     next_x(3) = wrapToPi(next_x(3));
+%     next_x(6) = wrapToPi(next_x(6));
     x(:,i) = next_x(end,:)';
 end
 
@@ -38,8 +38,8 @@ for i = 1:steps+1
     end
     y(:,i) = NL_MeasModel(x(:,i), vk);
     % wrap angle to [-pi pi]
-    y(1,i) = wrapToPi(y(1,i));
-    y(3,i) = wrapToPi(y(3,i));
+%     y(1,i) = wrapToPi(y(1,i));
+%     y(3,i) = wrapToPi(y(3,i));
 end
 
 end
