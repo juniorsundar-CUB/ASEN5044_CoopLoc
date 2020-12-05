f86e5157292ab518d4a2a12e82673583d7d22e09
Joshua Malburg <malburgj@gdls.com>
2020-12-05 15:25:26 -0800

    added switch to run MC or use mat data

--- MATLAB/Q6.m
+++ MATLAB/Q6.m
@@ -2,20 +2,31 @@
 close all, clearvars, clc
 load("cooplocalization_finalproj_KFdata.mat");
 
-x0 = [10 0 pi/2 -60 0 -pi/2]';
-u0 = [2 -pi/18 12 pi/25]';
+runMonteCarlo = false;
+if runMonteCarlo == true
+    x0 = [10 0 pi/2 -60 0 -pi/2]';
+    u0 = [2 -pi/18 12 pi/25]';
+    steps = 1000;
+    runs = 5;
+
+    Q_lkf = diag([0.0001 0.0001 0.01 0.1 0.1 0.01]);
+    Q_ekf = diag([.0015, .0015, 0.01, 0.001, 0.005, 0.01]);
+    P0 = diag([1 1 0.025 1 1 0.025]);
+else
+    x0 = [10 0 pi/2 -60 0 -pi/2]';
+    u0 = [2 -pi/18 12 pi/25]';
+    steps = size(ydata,2) - 2;
+    runs = 1;
+    Q_lkf = diag([0.0001 0.0001 0.01 0.1 0.1 0.01]);
+    Q_ekf = diag([.0015, .0015, 0.01, 0.001, 0.005, 0.01]);
+    P0 = diag([1 1 0.025 1 1 0.025]);
+end
+
+p = 5;
 Dt = 0.1;
 n = size(x0,1);
-steps = 1000;
 seed = 100;
 rng(seed);
-runs = 5;
-p = 5;
-
-Q_lkf = diag([0.0001 0.0001 0.01 0.1 0.1 0.01]);
-Q_ekf = diag([.0015, .0015, 0.01, 0.001, 0.005, 0.01]);
-P0 = diag([1 1 0.025 1 1 0.025]);
-
 
 EX_lkf = zeros(n, steps+1, runs);
 EY_lkf = zeros(p, steps+1, runs);
@@ -32,14 +43,25 @@ fig3 = figure;
 fig4 = figure;
 fig5 = figure;
 fig6 = figure;
-
+overlayAllRuns = true;
 for run = 1:runs
     disp(['run #', num2str(run)]);
-    
     %----------------------------------------------------------------------
-    % generate truth for Monte Carlo run
-    [x, y] = GenerateTruth(x0, u0, P0, Qtrue, Rtrue, Dt, steps, false);
-    t = (0:(length(x)-1))*Dt;
+    % generate data
+    if runMonteCarlo == true
+        [x, y] = GenerateTruth(x0, u0, P0, Qtrue, Rtrue, Dt, steps, false);
+        t = (0:(length(x)-1))*Dt;
+    else
+        y = ydata(:,2:end);
+        x = ones(size(x0,1),size(y,2));
+        x(:,1) = x0;
+        t = tvec(:,2:end);
+        steps = size(y,2) - 1;
+    end
+    if overlayAllRuns == true
+        PlotStates(fig1,t,x,'Ground Truth States, All Runs');
+        PlotMeasurements(fig2,t,y,'Ground Truth Measurements, All Runs');
+    end
 
     % assume we can get exact measurement noise from
     % specifications of sensors
@@ -52,7 +74,7 @@ for run = 1:runs
     % generate nominal trajectory for run, along with DT matrices
     [x_nom,y_nom] = GenerateNom(x0, u0, steps, Dt);
     [Fk, Hk, Ok] = GenerateLKFMats(u0, x_nom, steps+1, p, Dt);
-    dx_init = x(:,1) - x_nom(:,1);
+    dx_init = x0 - x_nom(:,1);
     P_init = eye(n);
     
     % Run filter for all time-steps of run #k
@@ -93,6 +115,15 @@ for run = 1:runs
     %----------------------------------------------------------------------
     %----------------------------------------------------------------------
     % Plot error during monte carlo runs
+    if overlayAllRuns == true
+        PlotStates(fig3,t,ex_lkf, ['LKF State Errors, Runs ',num2str(run)]);
+        PlotMeasurements(fig4,t,ey_lkf,['LKF Ground Truth Measurement Errors, Runs ',num2str(run)]);
+        PlotStates(fig5,t,ex_ekf, ['EKF State Errors, Runs ',num2str(run)]);
+        PlotMeasurements(fig6,t,ey_ekf,['EKF Ground Truth Measurement Errors, Runs ',num2str(run)]);
+    end
+end
+
+if overlayAllRuns == false
     PlotStates(fig1,t,x,'Ground Truth States, All Runs');
     PlotMeasurements(fig2,t,y,'Ground Truth Measurements, All Runs');
     PlotStates(fig3,t,ex_lkf, ['LKF State Errors, Runs ',num2str(run)]);
